21fcd247b4094fd40b2bf618fe7391525fce79ae
Figaarillo <axel.leonardi.22@gmail.com>
2024-08-14 05:33:12 -0300

    feat: create auth router

---
+++ internal/infrastructure/router/auth.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/Figaarillo/golerplate/internal/infrastructure/handler"
+	"github.com/gorilla/mux"
+)
+
+type AuthRouter struct {
+	router  *mux.Router
+	handler handler.AuthHandler
+}
+
+func NewAuthRouter(router *mux.Router, handler handler.AuthHandler) *AuthRouter {
+	subroutes := router.PathPrefix("/api/auth").Subrouter()
+
+	return &AuthRouter{
+		router:  subroutes,
+		handler: handler,
+	}
+}
+
+func (a *AuthRouter) SetupRoutes() {
+	a.router.HandleFunc("/register", a.handler.Register).Methods(http.MethodPost)
+}
