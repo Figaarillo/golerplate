7fb4c2c00921ccbf7af1b2ef64ff519eb33e7158
Figaarillo <axel.leonardi.22@gmail.com>
2024-08-14 05:20:28 -0300

    feat: add store and generate tokens methods in use case

--- internal/application/usecase/auth.go
+++ internal/application/usecase/auth.go
@@ -1,7 +1,7 @@
 package usecase
 
 import (
-	"log"
+	"fmt"
 	"time"
 
 	"github.com/Figaarillo/golerplate/internal/domain/entity"
@@ -15,12 +15,7 @@ type AuthUseCase struct {
 	repository repository.AuthRepository
 }
 
-func NewAuthUseCase(r repository.AuthRepository) *AuthUseCase {
-	env, err := config.NewEnvConf(".env")
-	if err != nil {
-		log.Fatalf("could not load config file: %v", err)
-	}
-
+func NewAuthUseCase(env *config.EnvVars, r repository.AuthRepository) *AuthUseCase {
 	return &AuthUseCase{
 		env:        env,
 		repository: r,
@@ -31,7 +26,7 @@ func (uc *AuthUseCase) GenerateAccessToken(userID entity.ID) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["user_id"] = userID
-	claims["exp"] = time.Now().Add(time.Second * time.Duration(uc.env.JWT_EXPIRATION))
+	claims["exp"] = time.Now().Add(time.Second * time.Duration(uc.env.JWT_EXPIRATION)).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(uc.env.JWT_SECRET_KEY))
@@ -40,8 +35,41 @@ func (uc *AuthUseCase) GenerateAccessToken(userID entity.ID) (string, error) {
 func (uc *AuthUseCase) GenerateRefreshToken(userID entity.ID) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["user_id"] = userID
-	claims["exp"] = time.Now().Add(time.Second * time.Duration(uc.env.JWT_REFRESH_EXPIRATION))
+	claims["exp"] = time.Now().Add(time.Second * time.Duration(uc.env.JWT_REFRESH_EXPIRATION)).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(uc.env.JWT_SECRET_KEY))
+}
+
+func (uc *AuthUseCase) StoreAccessToken(token entity.Token) error {
+	return uc.repository.StoreToken(token)
+}
+
+func (uc *AuthUseCase) GenerateAndStoreTokens(userId entity.ID) (entity.Token, error) {
+	var accessToken string
+	var refreshToken string
+	var err error
+
+	if accessToken, err = uc.GenerateAccessToken(userId); err != nil {
+		return entity.Token{}, fmt.Errorf("could not generate access token: %v", err)
+	}
+
+	if refreshToken, err = uc.GenerateRefreshToken(userId); err != nil {
+		return entity.Token{}, fmt.Errorf("could not generate refresh token: %v", err)
+	}
+
+	jwt := entity.Token{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+		ExpiresAt:    time.Now().Add(time.Second * time.Duration(uc.env.JWT_EXPIRATION)),
+		IssuedAt:     time.Now(),
+		TokenType:    "Bearer",
+		UserID:       userId,
+	}
+
+	if err = uc.StoreAccessToken(jwt); err != nil {
+		return entity.Token{}, fmt.Errorf("could not store access token: %v", err)
+	}
+
+	return jwt, nil
 }
