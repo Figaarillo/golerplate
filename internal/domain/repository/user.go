0c615f051ea6201ca4eb53a5b825331b41ee73d3
Figaarillo <axel.leonardi.22@gmail.com>
2024-08-16 20:39:59 -0300

    feat: add new method in user repository

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -5,6 +5,7 @@ import "github.com/Figaarillo/golerplate/internal/domain/entity"
 type UserRepository interface {
 	ListAll(offset, limit int) ([]entity.User, error)
 	GetByID(id entity.ID) (entity.User, error)
+	GetByProp(prop string, value interface{}) (entity.User, error)
 	Create(user *entity.User) (entity.User, error)
 	Update(id entity.ID, payload entity.User) (entity.User, error)
 	Delete(id entity.ID) error
